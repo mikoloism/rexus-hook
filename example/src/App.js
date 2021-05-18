d7ac0095b75dacf7e4041522d80548cd59450dc7
mikoloism <mikoloism.github@gmail.com>
2021-05-18 22:39:23 +0430

    Formate example/App.js

--- example/src/App.js
+++ example/src/App.js
@@ -8,34 +8,34 @@ import MyComponent from './components/my-component';
 // import providerTrux and createTrux
 import { Provider, createTrux } from 'react-trux';
 
-// import store (state, actions)
+// import store (states, actions)
 import * as Actions from './store/actions';
-import State from './store/state';
+import States from './store/states';
 
 function App() {
-	let Store = createTrux({ state: State, actions: Actions });
-	return (
-		<Provider store={Store}>
-			<div className='App'>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-					<p>
-						Edit <code>src/App.js</code> and save to reload. Edit file on{' '}
-						<code>src/store/</code> folder and save to reload.
-					</p>
-					<a
-						className='App-link'
-						href='https://github.com/miko-github/'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						About React-Trux
-					</a>
-				</header>
-				<MyComponent />
-			</div>
-		</Provider>
-	);
+  let Store = createTrux({ state: States, actions: Actions });
+  return (
+    <Provider store={Store}>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. Edit file on{' '}
+            <code>src/store/</code> folder and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://github.com/miko-github/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            About React-Trux
+          </a>
+        </header>
+        <MyComponent />
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
